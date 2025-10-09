51b84160b93b3c0a64ed9cb5b91d9cafd33afb46
Frost Ming <me@frostming.com>
2025-10-09 15:19:32 +0800

    refactor: remove validation checks for service discovery in routing modes
    Signed-off-by: Frost Ming <me@frostming.com>

--- sgl-router/src/main.rs
+++ sgl-router/src/main.rs
@@ -430,12 +430,6 @@ impl CliArgs {
         } else if self.pd_disaggregation {
             let decode_urls = self.decode.clone();
 
-            if !self.service_discovery && (prefill_urls.is_empty() || decode_urls.is_empty()) {
-                return Err(ConfigError::ValidationFailed {
-                    reason: "PD disaggregation mode requires --prefill and --decode URLs when not using service discovery".to_string(),
-                });
-            }
-
             RoutingMode::PrefillDecode {
                 prefill_urls,
                 decode_urls,
@@ -443,12 +437,6 @@ impl CliArgs {
                 decode_policy: self.decode_policy.as_ref().map(|p| self.parse_policy(p)),
             }
         } else {
-            if !self.service_discovery && self.worker_urls.is_empty() {
-                return Err(ConfigError::ValidationFailed {
-                    reason: "Regular mode requires --worker-urls when not using service discovery"
-                        .to_string(),
-                });
-            }
             RoutingMode::Regular {
                 worker_urls: self.worker_urls.clone(),
             }
